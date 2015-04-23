eed4e7b2f8a62314e29a5053907e7f42c921ce4b
euglazer <eugenestapler@gmail.com>
2015-04-24 00:01:31 +1200

    fix shit lol

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,12 +3,12 @@ class Scraper < ActiveRecord::Base
   def self.get_tracks(user_permalink, depth, track_ids)
     if depth > 0
       puts "#{depth} more to go!!!!"
-      first_liked_track = CLIENT.get("/users/#{user_permalink}/favorites").first
+      first_liked_track = CLIENT.get("/users/#{user_permalink}/favorites").sample
       first_liked_track_id = first_liked_track.id
       track_ids << first_liked_track_id
-      first_favoriter_permalink = CLIENT.get("/tracks/#{first_liked_track_id}/favoriters").first.permalink
+      random_favoriter_permalink = CLIENT.get("/tracks/#{first_liked_track_id}/favoriters").sample.permalink
       depth -= 1
-      self.get_tracks(first_favoriter_permalink, depth, track_ids)
+      self.get_tracks(random_favoriter_permalink, depth, track_ids)
     end
   end
 
